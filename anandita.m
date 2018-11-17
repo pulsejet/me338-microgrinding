2563bfb60430627165458cc68f0520c50cef0e30
Varun Patil <radialapps@gmail.com>
2018-11-18 03:44:39 +0530

    Complete Fy graphs

--- anandita.m
+++ anandita.m
@@ -32,9 +32,23 @@ function [] = anandita ()
   #nitin("./slot grinding/N/24000_30_50_slot.lvm", 85, 91, 0.05, M, 2, "F_y", 0, 1, 24000, 30, 50, tool);
   #nitin("./slot grinding/N/24000_30_60_slot.lvm", 88, 90, 0.05, M, 2, "F_y", 0, 1, 24000, 30, 60, tool);
   #nitin("./slot grinding/N/24000_60_20_slot.lvm", 46.2, 47.5, 0.05, M, 2, "F_y", 2, 1, 24000, 60, 20, tool);
-  #nitin("./slot grinding/N/24000_90_20_slot.lvm", 32.15, 33.5, 0.05, M, 2, "F_y", 1, 1, 24000, 90, 20, tool);
+  #nitin("./slot grinding/N/24000_90_20_slot.lvm", 31.15, 33.5, 0.05, M, 2, "F_y", 1, 1, 24000, 90, 20, tool);
   #nitin("./slot grinding/N/24000_120_20_slot.lvm", 25.3, 27, 0.05, M, 2, "F_y", 2, 1, 24000, 120, 20, tool);
   #nitin("./slot grinding/N/24000_150_20_slot.lvm", 22.1, 24, 0.05, M, 2, "F_y", 2, 1, 24000, 150, 20, tool);
+  
+  tool = "Metal Bonded";
+  #nitin("./slot grinding/Metal Bonded/07-11-2018/12000_30_20.lvm", 89, 90.5, 0.03, M, 3, "F_y", 2, 1, 12000, 30, 20, tool);
+  #nitin("./slot grinding/Metal Bonded/07-11-2018/24000_30_20.lvm", 89.5, 91, 0.03, M, 3, "F_y", -1, 1, 24000, 30, 20, tool);
+  #nitin("./slot grinding/Metal Bonded/07-11-2018/48000_30_20.lvm", 87.5, 89.2, 0.03, M, 3, "F_y", -1, 1, 48000, 30, 20, tool);
+  #nitin("./slot grinding/Metal Bonded/07-11-2018/60000_30_20.lvm", 86, 94, 0.03, M, 3, "F_y", 2, 1, 60000, 30, 20, tool);
+  #nitin("./slot grinding/Metal Bonded/07-11-2018/24000_30_30.lvm", 89, 92, 0.03, M, 3, "F_y", 2, 1, 24000, 30, 30, tool);
+  #nitin("./slot grinding/Metal Bonded/07-11-2018/24000_30_40.lvm", 90, 92, 0.03, M, 3, "F_y", 2, 1, 24000, 30, 40, tool);
+  #nitin("./slot grinding/Metal Bonded/07-11-2018/24000_30_50.lvm", 90, 92, 0.03, M, 3, "F_y", 0, 1, 24000, 30, 50, tool);
+  #nitin("./slot grinding/Metal Bonded/07-11-2018/24000_30_60.lvm", 90, 91.6, 0.03, M, 3, "F_y", 0, 1, 24000, 30, 60, tool);
+  #nitin("./slot grinding/Metal Bonded/07-11-2018/24000_60_20.lvm", 44.5, 47, 0.03, M, 3, "F_y", 2, 1, 24000, 60, 20, tool);
+  #nitin("./slot grinding/Metal Bonded/07-11-2018/24000_90_20.lvm", 33, 35, 0.03, M, 3, "F_y", 4, 1, 24000, 90, 20, tool);
+  #nitin("./slot grinding/Metal Bonded/07-11-2018/24000_120_20.lvm", 26.3, 27.6, 0.03, M, 3, "F_y", 0, 1, 24000, 120, 20, tool);
+  #nitin("./slot grinding/Metal Bonded/07-11-2018/24000_150_20.lvm", 25.4, 26.8, 0.03, M, 3, "F_y", 1, 1, 24000, 150, 20, tool);
   
 
   # Y-direction bare forces
@@ -48,8 +62,21 @@ function [] = anandita ()
   #abhinav("./slot grinding/N/24000_30_50_slot.lvm", 85, 8, M, 2, 10);
   #abhinav("./slot grinding/N/24000_30_60_slot.lvm", 88, 2, M, 2, 10);
   #abhinav("./slot grinding/N/24000_60_20_slot.lvm", 46.2, 2, M, 2, 10);
-  #abhinav("./slot grinding/N/24000_90_20_slot.lvm", 32.15, 2, M, 2, 10);
+  #abhinav("./slot grinding/N/24000_90_20_slot.lvm", 30, 20, M, 2, 10);
   #abhinav("./slot grinding/N/24000_120_20_slot.lvm", 25.3, 20, M, 2, 10);
   #abhinav("./slot grinding/N/24000_150_20_slot.lvm", 22.1, 20, M, 2, 10);
+  
+  #abhinav("./slot grinding/Metal Bonded/07-11-2018/12000_30_20.lvm", 89, 3, M, 3, 1);
+  #abhinav("./slot grinding/Metal Bonded/07-11-2018/24000_30_20.lvm", 89, 5, M, 3, 1);
+  #abhinav("./slot grinding/Metal Bonded/07-11-2018/48000_30_20.lvm", 87, 5, M, 3, 1);
+  #abhinav("./slot grinding/Metal Bonded/07-11-2018/60000_30_20.lvm", 86, 20, M, 3, 1);
+  #abhinav("./slot grinding/Metal Bonded/07-11-2018/24000_30_30.lvm", 89, 4, M, 3, 1);
+  #abhinav("./slot grinding/Metal Bonded/07-11-2018/24000_30_40.lvm", 90, 100, M, 3, 10);
+  #abhinav("./slot grinding/Metal Bonded/07-11-2018/24000_30_50.lvm", 90, 100, M, 3, 10);
+  #abhinav("./slot grinding/Metal Bonded/07-11-2018/24000_30_60.lvm", 90, 100, M, 3, 10);
+  #abhinav("./slot grinding/Metal Bonded/07-11-2018/24000_60_20.lvm", 44, 5, M, 3, 10);
+  #abhinav("./slot grinding/Metal Bonded/07-11-2018/24000_90_20.lvm", 33, 50, M, 3, 10);
+  #abhinav("./slot grinding/Metal Bonded/07-11-2018/24000_120_20.lvm", 20, 50, M, 3, 10);
+  #abhinav("./slot grinding/Metal Bonded/07-11-2018/24000_150_20.lvm", 10, 50, M, 3, 10);
 
 endfunction
