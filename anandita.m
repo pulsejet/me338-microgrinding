e3c3fb6079844c8e037a4da9016e6395c607b3b7
Varun Patil <radialapps@gmail.com>
2018-11-18 16:43:02 +0530

    Add Fz graphs

--- anandita.m
+++ anandita.m
@@ -9,9 +9,10 @@
 
 function [] = anandita ()
   M = 2;
+  
+  ###################################################
   tool = "Electroplated";
-
-  #nitin("./slot grinding/N/12000_30_20_slot.lvm", 135, 137, 0.01, M, 3, "F_x", 3, 1, 24000, 30, 20, tool);
+  #################### Fy ###########################
 
   #nitin("./slot grinding/N/12000_30_20_slot.lvm", 89.5, 91, 0.05, M, 2, "F_y", 10, 1, 12000, 30, 20, tool);
   #nitin("./slot grinding/N/24000_30_20_slot.lvm", 88.5, 90, 0.05, M, 2, "F_y", 1, 1, 24000, 30, 20, tool);
@@ -26,8 +27,27 @@ function [] = anandita ()
   #nitin("./slot grinding/N/24000_90_20_slot.lvm", 31.15, 33.5, 0.05, M, 2, "F_y", 1, 1, 24000, 90, 20, tool);
   #nitin("./slot grinding/N/24000_120_20_slot.lvm", 25.3, 27, 0.05, M, 2, "F_y", 2, 1, 24000, 120, 20, tool);
   #nitin("./slot grinding/N/24000_150_20_slot.lvm", 22.1, 24, 0.05, M, 2, "F_y", 2, 1, 24000, 150, 20, tool);
+
+  #################### Fz ###########################
   
+  #nitin("./slot grinding/N/12000_30_20_slot.lvm", 89.5, 91, 0.05, M, 3, "F_z", 9, 1, 12000, 30, 20, tool);
+  #nitin("./slot grinding/N/24000_30_20_slot.lvm", 88.5, 90, 0.05, M, 3, "F_z", 0, 1, 24000, 30, 20, tool);
+  #nitin("./slot grinding/N/36000_30_20_slot.lvm", 87.5, 90, 0.05, M, 3, "F_z", 1, 1, 36000, 30, 20, tool);
+  #nitin("./slot grinding/N/48000_30_20_slot.lvm", 87, 88.5, 0.05, M, 3, "F_z", 1, 1, 48000, 30, 20, tool);
+  #nitin("./slot grinding/N/60000_30_20_slot.lvm", 87.2, 88.5, 0.05, M, 3, "F_z", 0, 1, 60000, 30, 20, tool);
+  #nitin("./slot grinding/N/24000_30_30_slot.lvm", 87, 89, 0.05, M, 3, "F_z", 0, 1, 24000, 30, 30, tool);
+  #nitin("./slot grinding/N/24000_30_40_slot.lvm", 88, 91, 0.05, M, 3, "F_z", 2, 1, 24000, 30, 40, tool);
+  #nitin("./slot grinding/N/24000_30_50_slot.lvm", 85, 91, 0.05, M, 3, "F_z", -1, 1, 24000, 30, 50, tool);
+  #nitin("./slot grinding/N/24000_30_60_slot.lvm", 88, 90, 0.05, M, 3, "F_z", -1, 1, 24000, 30, 60, tool);
+  #nitin("./slot grinding/N/24000_60_20_slot.lvm", 46.2, 47.5, 0.05, M, 3, "F_z", 0, 1, 24000, 60, 20, tool);
+  #nitin("./slot grinding/N/24000_90_20_slot.lvm", 31.15, 33.5, 0.05, M, 3, "F_z", 0, 1, 24000, 90, 20, tool);
+  #nitin("./slot grinding/N/24000_120_20_slot.lvm", 25.3, 27, 0.05, M, 3, "F_z", -2, 1, 24000, 120, 20, tool);  
+  #nitin("./slot grinding/N/24000_150_20_slot.lvm", 22.1, 24, 0.05, M, 3, "F_z", -1, 1, 24000, 150, 20, tool);
+  
+  ###################################################
   tool = "Metal Bonded";
+  #################### Fy ###########################
+
   #nitin("./slot grinding/Metal Bonded/07-11-2018/12000_30_20.lvm", 89, 90.5, 0.03, M, 3, "F_y", 2, 1, 12000, 30, 20, tool);
   #nitin("./slot grinding/Metal Bonded/07-11-2018/24000_30_20.lvm", 89.5, 91, 0.03, M, 3, "F_y", -1, 1, 24000, 30, 20, tool);
   #nitin("./slot grinding/Metal Bonded/07-11-2018/48000_30_20.lvm", 87.5, 89.2, 0.03, M, 3, "F_y", -1, 1, 48000, 30, 20, tool);
@@ -40,6 +60,21 @@ function [] = anandita ()
   #nitin("./slot grinding/Metal Bonded/07-11-2018/24000_90_20.lvm", 33, 35, 0.03, M, 3, "F_y", 4, 1, 24000, 90, 20, tool);
   #nitin("./slot grinding/Metal Bonded/07-11-2018/24000_120_20.lvm", 26.3, 27.6, 0.03, M, 3, "F_y", 0, 1, 24000, 120, 20, tool);
   #nitin("./slot grinding/Metal Bonded/07-11-2018/24000_150_20.lvm", 25.4, 26.8, 0.03, M, 3, "F_y", 1, 1, 24000, 150, 20, tool);
+  
+  #################### Fz ###########################
+
+  #nitin("./slot grinding/Metal Bonded/07-11-2018/12000_30_20.lvm", 89, 90.5, 0.03, M, 2, "F_z", 2, 1, 12000, 30, 20, tool);
+  #nitin("./slot grinding/Metal Bonded/07-11-2018/24000_30_20.lvm", 89.5, 91, 0.03, M, 2, "F_z", 1, 1, 24000, 30, 20, tool);
+  #nitin("./slot grinding/Metal Bonded/07-11-2018/48000_30_20.lvm", 87.5, 89.2, 0.03, M, 2, "F_z", -1, 1, 48000, 30, 20, tool);
+  #nitin("./slot grinding/Metal Bonded/07-11-2018/60000_30_20.lvm", 86, 94, 0.03, M, 2, "F_z", 2, 1, 60000, 30, 20, tool);
+  #nitin("./slot grinding/Metal Bonded/07-11-2018/24000_30_30.lvm", 89, 92, 0.03, M, 2, "F_z", 6, 1, 24000, 30, 30, tool);
+  #nitin("./slot grinding/Metal Bonded/07-11-2018/24000_30_40.lvm", 90, 92, 0.03, M, 2, "F_z", 7, 1, 24000, 30, 40, tool);
+  nitin("./slot grinding/Metal Bonded/07-11-2018/24000_30_50.lvm", 90, 92, 0.03, M, 2, "F_z", 7, 1, 24000, 30, 50, tool);
+  #nitin("./slot grinding/Metal Bonded/07-11-2018/24000_30_60.lvm", 90, 91.6, 0.03, M, 2, "F_z", 5, 1, 24000, 30, 60, tool);
+  #nitin("./slot grinding/Metal Bonded/07-11-2018/24000_60_20.lvm", 44.5, 47, 0.03, M, 2, "F_z", 7, 1, 24000, 60, 20, tool);
+  #nitin("./slot grinding/Metal Bonded/07-11-2018/24000_90_20.lvm", 33, 35, 0.03, M, 2, "F_z", 7, 1, 24000, 90, 20, tool);
+  #nitin("./slot grinding/Metal Bonded/07-11-2018/24000_120_20.lvm", 26.3, 27.6, 0.03, M, 2, "F_z", 2, 1, 24000, 120, 20, tool);
+  #nitin("./slot grinding/Metal Bonded/07-11-2018/24000_150_20.lvm", 25.4, 26.8, 0.05, M, 2, "F_z", 5, 1, 24000, 150, 20, tool);
   
 
   # Y-direction bare forces
